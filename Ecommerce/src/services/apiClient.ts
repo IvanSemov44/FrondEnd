81c6a31149c458cf8c6c57ff18480bae69ed0a43
IvanSemov44 <ivan.semov.44@gmail.com>
2024-11-21 12:55:37 +0200

    created base apiClient

---
+++ Ecommerce/src/services/apiClient.ts
@@ -0,0 +1,22 @@
+import axios, { AxiosRequestConfig } from "axios";
+
+export interface FetchResponse<T> {
+  result: T[];
+}
+
+const axiosInstance = axios.create({
+  baseURL: "https://localhost:7117/api",
+});
+
+class ApiClient<T> {
+  endpoint: string;
+
+  constructor(endpoint: string) {
+    this.endpoint = endpoint;
+  }
+
+  getAll = (config: AxiosRequestConfig) =>
+    axiosInstance.get<T>(this.endpoint, config).then((res) => res.data);
+}
+
+export default ApiClient;
